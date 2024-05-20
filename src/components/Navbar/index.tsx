0c3a7876c3cf6f1270d0d4fd8025a004348f11ff
Patrycja Kalińska <59940332+patrycjakalinska@users.noreply.github.com>
2024-05-20 09:46:25 +0200

    Adjustments for Navbar with props (#7)
    For more managable Navbar we added following props to `<Navbar/>`:
     * `isAlgolia` - decides if there is a search engine in docs or not
     * `isToggle` - decides if there is a theme switch in docs

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,16 +4,20 @@ import NavbarContent from './Content';
 export interface NavbarProps {
   heroImages?: { logo: string; title?: string };
   titleImages?: { light: string; dark: string };
+  isAlgoliaActive?: boolean
+  isThemeSwitcherShown?: boolean
 }
 
 export function Navbar({
   heroImages,
   titleImages,
+  isAlgoliaActive,
+  isThemeSwitcherShown
 }: NavbarProps
 ) {
   return (
     <NavbarLayout>
-      <NavbarContent heroImages={heroImages} titleImages={titleImages} />
+      <NavbarContent isThemeSwitcherShown={isThemeSwitcherShown} isAlgoliaActive={isAlgoliaActive} heroImages={heroImages} titleImages={titleImages} />
     </NavbarLayout>
   );
 }
