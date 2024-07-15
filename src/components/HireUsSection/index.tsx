f454b53874e7084a0082b4bd1c1a76216fac8a67
Patrycja Kalińska <cysiak0412@gmail.com>
2024-07-15 14:00:31 +0200

    Add Hire Us Section

---
+++ src/components/HireUsSection/index.tsx
@@ -0,0 +1,38 @@
+import {
+  ButtonStyling,
+  BorderStyling,
+  HomepageButton,
+} from '../HomepageButton';
+import styles from './styles.module.css';
+
+export const HireUsSection = ({ content }: { content?: string }) => {
+  return (
+    <div className={styles.hireUsSectionWrapper}>
+      <div className={styles.hireUsTitleContainer}>
+        <h2>
+          We are <span>Software Mansion</span>
+        </h2>
+      </div>
+      <p className={styles.hireUsSectionBody}>
+        {content ? (
+          content
+        ) : (
+          <>
+            React Native Core Contributors and experts in dealing with all kinds
+            of React Native issues. No matter if you need help with gestures or
+            React Native development we can help.
+          </>
+        )}
+      </p>
+
+      <div className={styles.hireUsButton}>
+        <HomepageButton
+          href="https://swmansion.com/contact/projects?utm_source=gesture-handler&utm_medium=docs"
+          title="Hire us"
+          backgroundStyling={ButtonStyling.SECTION}
+          borderStyling={BorderStyling.SECTION}
+        />
+      </div>
+    </div>
+  );
+};
