0f8a45835f789fdc3c2d6e3b4cf8a3eb4742510f
Patrycja Kalińska <cysiak0412@gmail.com>
2024-07-24 11:21:18 +0200

    Init

---
+++ packages/docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/marcysia/swm/repos/t-rex-ui/docs/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/marcysia/swm/repos/t-rex-ui/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/marcysia/swm/repos/t-rex-ui/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/marcysia/swm/repos/t-rex-ui/docs/src/css/index.css'),
+];
