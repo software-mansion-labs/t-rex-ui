3983822a9d4edb2d6f9a214da787f9f4c980be5e
Patrycja Kalińska <cysiak0412@gmail.com>
2024-07-24 16:35:07 +0200

    formatting

--- packages/t-rex-ui/src/main.ts
+++ packages/t-rex-ui/src/main.ts
@@ -1,24 +1,24 @@
-export { Admonition } from './components/Admonition';
-export { PaginatorNavLink } from './components/PaginatorNavLink';
+export { Admonition } from './components/Admonition'
+export { PaginatorNavLink } from './components/PaginatorNavLink'
 
-export { Navbar } from './components/Navbar';
-export { Footer } from './components/Footer';
+export { Navbar } from './components/Navbar'
+export { Footer } from './components/Footer'
 
 // export { LogoWrapper } from './components/Logo'
-export { ColorModeToggle } from './components/ColorModeToggle';
-export { ThemedImage } from './components/ThemedImage';
+export { ColorModeToggle } from './components/ColorModeToggle'
+export { ThemedImage } from './components/ThemedImage'
 
-export { DocSidebarItems } from './components/DocSidebarItems';
-export { DocCard } from './components/DocCard';
-export { DocSidebar } from './components/DocSidebar';
-export { DocSidebarItem } from './components/DocSidebarItem';
-export { DocItemTOCMobile } from './components/DocItem/TOC/Mobile';
-export { DocItemMetadata } from './components/DocItem/Metadata';
+export { DocSidebarItems } from './components/DocSidebarItems'
+export { DocCard } from './components/DocCard'
+export { DocSidebar } from './components/DocSidebar'
+export { DocSidebarItem } from './components/DocSidebarItem'
+export { DocItemTOCMobile } from './components/DocItem/TOC/Mobile'
+export { DocItemMetadata } from './components/DocItem/Metadata'
 
-export { TOCCollapsible } from './components/TOCCollapsible';
-export { TOCItems } from './components/TOCItems';
-export { TOCItemTree } from './components/TOCItems/Tree';
+export { TOCCollapsible } from './components/TOCCollapsible'
+export { TOCItems } from './components/TOCItems'
+export { TOCItemTree } from './components/TOCItems/Tree'
 
-export { SearchPage } from './components/SearchPage';
+export { SearchPage } from './components/SearchPage'
 
-export { HireUsSection } from './components/HireUsSection';
+export { HireUsSection } from './components/HireUsSection'
